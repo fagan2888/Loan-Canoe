1afa0c9483016387de39c420c8196e07a9c0953e
NO_REPLY <bblakezen12@gmail.com>
2019-08-30 01:01:38 -0400

    WRANGLING: updated lines for balanc rand sampl all yrs

---
+++ notebooks/00_sql/03b_interim_wrangle__balanced_rand_samp__bbz.sql
@@ -0,0 +1,25 @@
+/***************************************************************************************************************/
+/* Purpose:  (1) Typecast transform & Generate Simple, Balanced, Random Samples for each year for ingestion    */
+/*               + (2) Union all the samples for the interim dataset for additional wrangling with pandas      */                                                                                           */
+/*                                                                                                             */
+/* Author: Blake Zenuni, Summer 2019                                                                           */
+/* Date Created:  Aug 1, 2019                                                                                  */
+/* Last Modified: Aub 29, 2019                                                                                 */
+/*                                                                                                             */
+/***************************************************************************************************************/
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+      --> NB: Latin abbreviation for NOTA BENE, meaning "note well" <--
+/*---------------------------------------------------------------------------------------------------------*/
+
+
+ -- NB1: In this SQL script, simple random samples are balanced for outcomes (50/50 loans approved vs. loans denied).
+ -- NB2: Script 03a applies this same logic, but for unbalanced outcomes.
+
+
+
+
+/*======================== 03b. Simple Random Samples (Balanced Outcomes) for HMDA 2009-2010 =========================*/
+
+/*** =========================================== END 03a - SQL Script  ============================================ ***/
