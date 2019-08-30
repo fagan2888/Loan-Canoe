f216b98195c73651aba75cc88d7f0115626dee9c
NO_REPLY <bblakezen12@gmail.com>
2019-08-30 01:00:32 -0400

    WRANGLING: updated lines for unbal strata rand samp all yrs

--- notebooks/00_sql/04a_interim_wrangle__unbal_strat_rand_samp__bbz.sql
+++ notebooks/00_sql/04a_interim_wrangle__unbal_strat_rand_samp__bbz.sql
@@ -1,12 +1,12 @@
-/*****************************************************************************************************************/
-/* Purpose:  (1) Typecast transform & Generate Stratified, Unbalanced Random Samples for each year for ingestion */
-/*               + (2) Union all the samples for the interim dataset for additional wrangling with pandas        */                                                                                           */
-/*                                                                                                               */
-/* Author: Blake Zenuni, Summer 2019                                                                             */
-/* Date Created:  Aug 1, 2019                                                                                    */
-/* Last Modified: Aub 29, 2019                                                                                   */
-/*                                                                                                               */
-/*****************************************************************************************************************/
+/******************************************************************************************************************/
+/* Purpose:  (1) Typecast transform & Generate Stratified, Unbalanced, Random Samples for each year for ingestion */
+/*               + (2) Union all the samples for the interim dataset for additional wrangling with pandas         */                                                                                           */
+/*                                                                                                                */
+/* Author: Blake Zenuni, Summer 2019                                                                              */
+/* Date Created:  Aug 1, 2019                                                                                     */
+/* Last Modified: Aub 29, 2019                                                                                    */
+/*                                                                                                                */
+/******************************************************************************************************************/
 
 
 /*---------------------------------------------------------------------------------------------------------*/
@@ -14,12 +14,19 @@
 /*---------------------------------------------------------------------------------------------------------*/
 
 
- -- NB1: Stratified random samples are bucketed by household income (strata) + balanced two ways (see below).
- -- NB2: This script is for unbalanced outcomes; script 04b applies this same logic but for balanced outcome
- -- NB3: Bal
+ -- NB1: In this SQL script, stratified random samples are unbalanced for outcomes (see script 04b for balanced).
+
+ -- NB2: This random sample subset of the data is stratified by median household income:
+       --> i.   We divide the population into different household median income subgroups, or strata;
+       --> ii.  We apply statistical measurements of perentiles validate our strata (see below in CTEs);
+       --> iii. Random samples are taken from each percentile in proportion to the population, from each of the strata.
+
+    --> Documentation:
+             --http://www.wagonhq.com/sql-tutorial/calculating-percentiles-sql
+             --https://blog.usejournal.com/creating-an-unbiased-test-set-for-your
+--                     -model-using-stratified-sampling-technique-672b778022d5
 
 
+/*========================== 04b. Stratified Random Samples for HMDA 2009-2010 =============================*/
+/*============================================= END 04b - SQL Script  ================================================*/
 
-
-/*================================ 04a. Stratified Random samples for HMDA 2009-2010 =================================*/
-/*============================================= END 03a - SQL Script  ================================================*/
